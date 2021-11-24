389f142a662e647505cb973a4fa288459f4d4b74
Ricardo Serrano <ricardo.ss@live.com>
2021-11-24 11:39:29 -0600

    feat: State Boolean example

--- src/exercise/stateBoolean.jsx
+++ src/exercise/stateBoolean.jsx
@@ -1,10 +1,26 @@
 import { useState } from 'react'
 import '../App.css'
 
+const Switch = ({initialState}) =>{
+  const [isEnabled, setIsEnabled] = useState(initialState);
+  const handleClick = () => { setIsEnabled(!isEnabled)}
+  
+  const boxStyle = 'box ' + (isEnabled ? 'verde' : 'rojo')
+  return (
+    <>
+      <div className={boxStyle}>
+        <strong> State: {isEnabled.toString()} </strong>
+      </div>
+      <button onClick={handleClick}>{isEnabled ? 'Disable' : 'Enable'}</button>
+    </>
+  )
+}
 function StateBoolean() {
+
+
   return (
     <div className='App'>
-      Boolean
+      <Switch initialState={false}/>
     </div>
   )
 }
