fceb8c6d3cffb37385674badabe2ebd4ea058f80
Ricardo Serrano <ricardo.ss@live.com>
2021-11-24 14:42:16 -0600

    feat: Create person form

--- src/exercise/stateObject.jsx
+++ src/exercise/stateObject.jsx
@@ -1,8 +1,40 @@
+import {useState} from 'react'
+
+const initialValue = {
+    name: 'Ricardo',
+    surname: 'Serrano',
+    age: 24
+}
+
+const PersonForm = () => {
+    const [person, setPeson] = useState(initialValue)
+    console.log(person);
+
+    return(
+        <form>
+            <div>
+                <label htmlFor="">Name: </label>
+                <input type="text" name="name" id=""/>
+            </div>
+            <div>
+                <label htmlFor="">Surname: </label>
+                <input type="text" name="name" id=""/>
+            </div>
+            <div>
+                <label htmlFor="">Age: </label>
+                <input type="number" name="name" id="" min="0" max="130"/>
+            </div>
+            <div>
+                <button>Send Data</button>
+            </div>
+        </form>
+    )
+}
 
 const stateObject = () => {
     return(
-        <div>
-            State Object
+        <div className='App'>
+            <PersonForm />
         </div>
     )
 }
