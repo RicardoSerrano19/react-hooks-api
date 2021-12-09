af2a7666a025f24308d078314afe324e6bef377e
Ricardo Serrano <ricardo.ss@live.com>
2021-12-09 11:56:33 -0600

    feat: useEffect only once with addEventListener

---
+++ src/exercise/useEffect/effectOnlyOnce.jsx
@@ -0,0 +1,25 @@
+import {useState, useEffect} from 'react'
+
+
+export default function EffectOnlyOnce() {
+    const [x, setX] = useState(0);
+    const [y, setY] = useState(0);
+
+    console.log('- EffectOnlyOnce: render')
+    useEffect(() => {
+        console.log('- EffectOnlyOnce: useEffect (function, null')
+        window.addEventListener('mousemove', logMousePosition)
+    }, [])
+
+    const logMousePosition = event => {
+        console.log('- EffectOnlyOnce: MouseEvent')
+        setX(event.clientX)
+        setY(event.clientY)
+    }
+
+    return (
+        <div>
+            X = {x} | Y = {y}
+        </div>
+    )
+}
