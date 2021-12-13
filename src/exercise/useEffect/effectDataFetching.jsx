11e76d8b9169276eb37f960d087885c9dd23f262
Ricardo Serrano <ricardo.ss@live.com>
2021-12-13 15:13:09 -0600

    feat: Add form to fetch only one post

--- src/exercise/useEffect/effectDataFetching.jsx
+++ src/exercise/useEffect/effectDataFetching.jsx
@@ -4,22 +4,45 @@ import axios from 'axios'
 const URL = 'https://jsonplaceholder.typicode.com';
 
 export default function EffectDataFetching() {
-    const [posts, setPosts] = useState([]);
+    const [posts, setPosts] = useState({});
+    const [id, setId] = useState(1);
+    const [idFromBtn, setIdFromBtn] = useState(1);
 
     useEffect(() => {
-        console.log('- DataFetch: useEffect (function, [])')
-        axios.get(`${URL}/posts`)
+        console.log('- DataFetch: useEffect (function, [idFromBtn])')
+        axios.get(`${URL}/posts/${idFromBtn}`)
             .then(res => {
                 setPosts(res.data);
             })
             .catch(err => {
                 console.log(err)
             })
-    }, [])
+    }, [idFromBtn])
+
+
+    const onChangeId = (e) => {
+        const id = e.target.value;
+        setId(id);
+    }
+
+    const handleClick = () => {
+        setIdFromBtn(id);
+    }
 
     return (
         <div className='App'>
-            {posts.map(post => (
+            <input type="text" value={id} onChange={onChangeId}/>
+            <button onClick={handleClick}>Search</button>
+            <div className="post row" key={posts.id}>
+                <div className="user">
+                    <div className="avatar">{posts.userId}</div>
+                </div>
+                <div className="content">
+                    <div className="title">{posts.title}</div>
+                    <div className="body">{posts.body}</div>
+                </div>
+            </div>
+            {/*posts.map(post => (
                 <div className="post row" key={post.id}>
                     <div className="user">
                         <div className="avatar">{post.userId}</div>
@@ -29,7 +52,7 @@ export default function EffectDataFetching() {
                         <div className="body">{post.body}</div>
                     </div>
                 </div>
-            ))}
+            ))*/}
         </div>
     )
 }
