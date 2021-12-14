cd1a9883bd1309ff48f6162791ecab348fb2a59b
Ricardo Serrano <ricardo.ss@live.com>
2021-12-14 14:50:51 -0600

    feat: Add preferencesContext

--- src/exercise/useContext/contextUserInfo.jsx
+++ src/exercise/useContext/contextUserInfo.jsx
@@ -1,12 +1,15 @@
 import { useContext } from 'react'
-import { UserContext } from './contextUsername'
+import { UserContext, PreferencesContext } from './contextUsername'
 
 function UserInfo() {
     const userName = useContext(UserContext)
+    const preferences = useContext(PreferencesContext)
+
     console.log('- UserInfo: render')
     return (
         <span>
-            UserName context: {userName}
+            <p>UserName context: {userName} </p>
+            <p>Preferences context: {preferences}</p>
         </span>
     )
 }
--- src/exercise/useContext/contextUsername.jsx
+++ src/exercise/useContext/contextUsername.jsx
@@ -2,17 +2,20 @@ import {createContext, useState} from 'react'
 import Layout from './contextLayout'
 
 export const UserContext = createContext('Unknown')
+export const PreferencesContext = createContext('light-mode')
 export default function ContextUsername() {
     const [userName, setUserName] = useState('Ricardo Serrano');
     console.log('- ContextUsername: render')
 
     return (
         <div className='App'>
-            <UserContext.Provider value={userName}>
-                <Layout>
-                    Main Content
-                </Layout>
-            </UserContext.Provider>
+            <PreferencesContext.Provider>
+                <UserContext.Provider value={userName}>
+                    <Layout>
+                        Main Content
+                    </Layout>
+                </UserContext.Provider>
+            </PreferencesContext.Provider>
         </div>
     )
 }
