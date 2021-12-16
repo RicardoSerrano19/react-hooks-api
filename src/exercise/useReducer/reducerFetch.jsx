655bc48858908c24ba4fc1f93c2ea94bb8efefb4
Ricardo Serrano <ricardo.ss@live.com>
2021-12-16 15:18:17 -0600

    feat: Add useReducer to refactor states

--- src/exercise/useReducer/reducerFetch.jsx
+++ src/exercise/useReducer/reducerFetch.jsx
@@ -1,31 +1,41 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect, useReducer} from 'react'
 import axios from 'axios'
 
 const URL = 'https://jsonplaceholder.typicode.com';
+const initialState = {
+    loading: true,
+    error: '',
+    post: {}
+}
+
+const dataReducer = (state, action) => {
+    switch(action.type){
+        case 'SUCCESS':
+            return {loading: false, error: '', post: action.payload}
+        case 'ERROR':
+            return {loading: false, error: action.error, post: {}}
+        default:
+            initialState
+    }
+}
 
 const ReducerFetch = () => {
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState('');
-    const [post, setPost] = useState({})
+    const [state, dispatch] = useReducer(dataReducer, initialState)
 
     useEffect(() => {
-        axios.get(`${URL}/posts/dasd`)
+        axios.get(`${URL}/posts/1s`)
         .then(response => {
-            setLoading(false)
-            setPost(response.data)
-            setError('')
+            dispatch({type:'SUCCESS', payload: response.data})
+            console.log(response.data)
         }).catch(error =>{
-            setLoading(false)
-            setPost({})
-            setError('Something went wrong!')
-            console.log(error)
+            dispatch({type:'ERROR', error: error + ' - Something went wrong REDUCER!'})
         })
     }, [])
 
     return (
         <div className='App'>
-            {loading ? 'loading' : post.title}
-            {error ? error : null}
+            {state.loading ? 'loading' : state.post.title}
+            {state.error ? state.error : null}
         </div>
     )
 }
