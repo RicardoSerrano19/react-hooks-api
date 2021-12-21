97cfdf3fa8d46e36065c5e085c8dd903d18bec10
Ricardo Serrano <ricardo.ss@live.com>
2021-12-21 14:54:03 -0600

    feat: Difference between useRef and useState

--- src/exercise/useRef/refUpdate.jsx
+++ src/exercise/useRef/refUpdate.jsx
@@ -3,15 +3,23 @@ import React, {useRef, useState} from 'react'
 const RefUpdate = () => {
     console.log('RefUpdate: render');
     const countRef = useRef(0);
+    const [count, setCount] = useState(0);
 
     const updateReference = () => {
         countRef.current++;
         console.log(`Clicked ${countRef.current} times`);
     }
 
+    const updateState = () => {
+        const updateCount = count + 1;
+        console.log(`Clicked ${updateCount} times`);
+        setCount(updateCount);
+    }
+
     return (
         <div className='App'>
-            <button onClick={updateReference}>Click me</button>
+            <button onClick={updateReference}>Click me - useRef</button>
+            <button onClick={updateState}>Click me - useState</button>
         </div>
     )
 }
